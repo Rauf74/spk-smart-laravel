745332a6f3fccf2e8c8276a66b4e81ee219df947
Rauf <alfarras74@gmail.com>
2026-01-20 13:24:51 +0700

    chore: update seeders to match pg_neon.sql and add .sql to gitignore
    - Update KriteriaSeeder: 5 kriteria, K5 bobot 10%
    - Update UserSeeder: all 11 users from original database
    - Add *.sql to .gitignore

--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Kriteria;
 
 /**
  * Seeder untuk data Kriteria.
+ * Data sesuai dengan spk_smart_pg_neon.sql
  */
 class KriteriaSeeder extends Seeder
 {
@@ -17,8 +18,7 @@ class KriteriaSeeder extends Seeder
             ['id_kriteria' => 2, 'kode_kriteria' => 'K2', 'nama_kriteria' => 'Prestasi Akademik', 'jenis' => 'Benefit', 'bobot' => 25.00],
             ['id_kriteria' => 3, 'kode_kriteria' => 'K3', 'nama_kriteria' => 'Motivasi dan Kesiapan Mental', 'jenis' => 'Benefit', 'bobot' => 20.00],
             ['id_kriteria' => 4, 'kode_kriteria' => 'K4', 'nama_kriteria' => 'Kepribadian dan Kecocokan Psikologis', 'jenis' => 'Benefit', 'bobot' => 15.00],
-            ['id_kriteria' => 5, 'kode_kriteria' => 'K5', 'nama_kriteria' => 'Kemampuan Interpersonal dan Soft Skills', 'jenis' => 'Benefit', 'bobot' => 5.00],
-            ['id_kriteria' => 22, 'kode_kriteria' => 'K6', 'nama_kriteria' => 'Tester K', 'jenis' => 'Cost', 'bobot' => 5.00],
+            ['id_kriteria' => 5, 'kode_kriteria' => 'K5', 'nama_kriteria' => 'Kemampuan Interpersonal dan Soft Skills', 'jenis' => 'Benefit', 'bobot' => 10.00],
         ];
 
         foreach ($kriterias as $kriteria) {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,7 +7,7 @@ use App\Models\User;
 
 /**
  * Seeder untuk data user (Guru BK dan Siswa).
- * Data diambil dari database lama spk_dbcoba_v4.
+ * Data sesuai dengan spk_smart_pg_neon.sql
  */
 class UserSeeder extends Seeder
 {
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
                 'id_user' => 1,
                 'nama_user' => 'Guru BK',
                 'jenis_kelamin' => 'Laki-laki',
-                'username' => 'gurubk',
+                'username' => 'gurubk1',
                 'password' => bcrypt('123456'),
                 'role' => 'Guru BK',
                 'nis' => null,
@@ -62,6 +62,56 @@ class UserSeeder extends Seeder
                 'password' => bcrypt('siswa123'),
                 'role' => 'Siswa',
                 'nis' => '12348',
+                'is_logged_in' => false,
+            ],
+            [
+                'id_user' => 6,
+                'nama_user' => 'Dafit Muttaqin',
+                'jenis_kelamin' => 'Laki-laki',
+                'username' => 'siswa5',
+                'password' => bcrypt('siswa123'),
+                'role' => 'Siswa',
+                'nis' => '12349',
+                'is_logged_in' => false,
+            ],
+            [
+                'id_user' => 7,
+                'nama_user' => 'Danny Bungai',
+                'jenis_kelamin' => 'Laki-laki',
+                'username' => 'siswa240',
+                'password' => bcrypt('1234567'),
+                'role' => 'Guru BK',
+                'nis' => '12349',
+                'is_logged_in' => false,
+            ],
+            [
+                'id_user' => 8,
+                'nama_user' => 'chihuahuaw',
+                'jenis_kelamin' => 'Laki-laki',
+                'username' => 'wawa',
+                'password' => bcrypt('1234567'),
+                'role' => 'Siswa',
+                'nis' => '12222',
+                'is_logged_in' => false,
+            ],
+            [
+                'id_user' => 10,
+                'nama_user' => 'wwwwwwwww',
+                'jenis_kelamin' => null,
+                'username' => 'wwwwww',
+                'password' => bcrypt('123456'),
+                'role' => 'Siswa',
+                'nis' => null,
+                'is_logged_in' => false,
+            ],
+            [
+                'id_user' => 11,
+                'nama_user' => 'Rauf',
+                'jenis_kelamin' => 'Laki-laki',
+                'username' => 'eunzoo',
+                'password' => bcrypt('123456'),
+                'role' => 'Guru BK',
+                'nis' => null,
                 'is_logged_in' => false,
             ],
             [
